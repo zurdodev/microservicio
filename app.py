fabeae7d24be212b15f935d0357110e5c7b3736b
zurdodev <zurdophpbasket@gmail.com>
2026-04-12 14:43:24 -0600

    Solo cambio las líneas finales

--- app.py
+++ app.py
@@ -130,6 +130,6 @@ def pdf2json():
 
     return jsonify(orden_compra)
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
+
